731e0a255c03480647f7e562e5e6f640fa5e0220
EemanNaseer1 <eemannaseer1@gmail.com>
2024-08-19 08:53:18 -0700

    Chat App

--- lib/track_screen.dart
+++ lib/track_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/screen1.dart';
 import 'package:flutter/material.dart';
 
 class TrackScreen1 extends StatelessWidget {
@@ -7,12 +8,34 @@ class TrackScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color.fromRGBO(255, 255, 255, 1),
-        width: MediaQuery.of(context).size.width,
+       color: Color.fromRGBO(255, 255, 255, 1), 
+       width: MediaQuery.of(context).size.width,
         height:  MediaQuery.of(context).size.height,
         child: Column(
           children: [
-            
+            SizedBox(height: 60,),
+            Image.asset("assets/images/sweet.jpg"),
+            Column(
+              children: [
+                SizedBox(height: 20,),
+                Text("Track your  Comfort",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w600),),
+                Text("Food here",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w600),),
+                SizedBox(height: 15,),
+                Text("Here You Can find a chef or dish for every"),
+                Text("taste and color. Enjoy!"),
+                SizedBox(height: 90,),
+                Container(
+                  color: Color.fromRGBO(236, 37, 120, 1),
+                  height: 40,
+                  width: 160,
+                  child: TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Screen1()));
+                  }, child: Text("Next",style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),)),
+                ),
+                SizedBox(height: 35,),
+                Image.asset("assets/images/line.jpg")
+              ],
+            )
           ],
         ),
       )
